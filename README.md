d12768be09b1e92b0916952ea73e422fd5ed6bb8
Antony Jones <aj@desirableobjects.co.uk>
2017-05-10 14:39:30 +0100

    fix ratio for displays

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fit Text
+
+A simple library which resizes text to fit its container.
+
+No dependencies, just vanilla JS.
+
+Based on [Davatron5000's Fit Text JQuery plugin](https://github.com/davatron5000/FitText.js)
+
+```
+npm install fit-text
+```
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 class FlowText {
-  constructor (element, options) {
+  constructor (element, options = {}) {
     this.element = element
-    Object.assign({
+    this.settings = Object.assign({
         maximum   : 9999,
         minimum   : 1,
         maxFont   : 9999,
         minFont   : 1,
-        fontRatio : 35
+        fontRatio : 4
     }, options)
     window.addEventListener('resize', () => { this.reflow() }, true)
     this.reflow()
   }
 
   reflow () {
-    const { height, width } = element.getBoundingClientRect()
+    const { width } = this.element.getBoundingClientRect()
     const { minimum, maximum, fontRatio, minFont, maxFont } = this.settings
     const newWidth = width > maximum ? maximum : width < minimum ? minimum : width
     const fontBase = width / fontRatio
     const fontSize = fontBase > maxFont ? maxFont : fontBase < minFont ? minFont : fontBase
+    console.log(fontSize)
     this.element.style['font-size'] = `${fontSize}px`
   }
 
